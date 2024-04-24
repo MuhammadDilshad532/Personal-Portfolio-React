d0afb7e3f37e704ee1192c548f9eac29d3fd0568
Muhammad Dilshad <dilshadahamad532280@gmail.com>
2024-04-24 13:50:48 +0500

    install dependencys

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -26,10 +26,17 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Voluptatum eveniet vel quidem
+        consequuntur nisi incidunt laudantium, dolorem cupiditate. Possimus
+        officiis laborum quo laboriosam molestiae blanditiis quia nisi commodi
+        incidunt quas? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Consectetur voluptatibus, beatae iusto, quisquam minus at vero deleniti
+        nam reprehenderit excepturi quo aut quod odio quos non nesciunt totam
+        laboriosam! Sequi?
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
