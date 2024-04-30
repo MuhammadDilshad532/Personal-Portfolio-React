1061fa0cc96c66f96bbe84343d3226a15eedf45d
Muhammad Dilshad <dilshadahamad532280@gmail.com>
2024-04-30 23:27:04 +0500

    footer update

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,20 +3,18 @@ import footer from "../../assets/img/footer.png";
 import Github from "../../assets/img/github.png";
 import LinkedIn from "../../assets/img/linkedin.png";
 import Instagram from "../../assets/img/instagram.png";
+
 const Footer = () => {
   return (
-    <div class="">
-      <div class=" flex   relative  ">
-        <span className="mt-48 ml-[35%] text-white text-xl ">
-          dilshadahamad532280@gmail.com
-        </span>
-
-        <img src={footer} alt="" className=" absolute flex  -z-10" />
-        <div className="  text-white  flex  gap-10">
-          <img src={Github} alt="Github" className="w-11 h-11 mt-80" />
-          <img src={LinkedIn} alt="LinkedIn" className="w-11 h-11 mt-80" />
-          <img src={Instagram} alt="Instagram" className="w-11 h-11 mt-80" />
-        </div>
+    <div className="flex flex-col items-center relative">
+      <span className="text-white text-xl mt-48">
+        dilshadahamad532280@gmail.com
+      </span>
+      <img src={footer} alt="" className="absolute mt-1 -z-10" />
+      <div className="text-white flex gap-10 mt-24">
+        <img src={Github} alt="Github" className="w-11 h-11" />
+        <img src={LinkedIn} alt="LinkedIn" className="w-11 h-11" />
+        <img src={Instagram} alt="Instagram" className="w-11 h-11" />
       </div>
     </div>
   );
--- src/components/navBar/Header.jsx
+++ src/components/navBar/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
 
   return (
     <div className=" ">
-      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+      <div className="md:flex items-center justify-between  py-4 md:px-10 px-7">
         {/* logo section */}
         <div className="font-bold text-2xl cursor-pointer flex items-center gap-10">
           <span>Muhammad Dilshad</span>
@@ -30,7 +30,7 @@ const Header = () => {
         </div>
         {/* link items */}
         <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static h-12 rounded-2xl bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
             open ? "top-12" : "top-[-490px]"
           }`}
         >
